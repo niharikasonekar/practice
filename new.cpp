ff3660fa45983a35a9cbc29bad3b85b407f9c324
niharikasonekar <niharika151196@gmil.com>
2024-12-09 17:33:42 +0530

    moon

---
+++ new.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num= 2;
+    int power=3;
+    int ans=1;
+    for(int i=i;1<=3;++i)
+    {
+        ans=ans*num;
+    }
+    cout<<ans;
+    }
+
+}
+
