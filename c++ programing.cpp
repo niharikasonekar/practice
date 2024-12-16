b9b2533f44546f783fb9a01f437ed30c46cf6a84
niharikasonekar <niharika151196@gmil.com>
2024-12-16 17:39:19 +0530

    number

--- c++ programing.cpp
+++ c++ programing.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-for(int i=1;i<=10;i++){
+    int num=1;
+for(int i=1;i<=5;i++){
 for(int j=1;j<=i;j++){
-    cout<<i;
+    cout<<num;
+    num++;
 }
 cout<<endl;
 }
